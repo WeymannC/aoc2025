5d3fd3d31eb8b19cff690b7b9d7581d456ae2ba7
Christian Weymann <christian.weymann@tngtech.com>
2025-12-01 08:31:52 +0100

    Day 1 part 1

--- 1-1.py
+++ 1-1.py
@@ -1,6 +1,34 @@
 from aocd import get_data
 
 data = get_data(day=1, year=2025)
-example = r""""""
+example = r"""L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
 
 used_input = data
+starting_position = 50
+
+def parse_input(raw_input: str) -> list[int]:
+    steps = []
+    for instruction in raw_input.split("\n"):
+        if instruction[0] == "R":
+            steps.append(int(instruction[1:]))
+        else:
+            steps.append(-int(instruction[1:]))
+    return steps
+
+position = starting_position
+total = 0
+for instruction in parse_input(used_input):
+    position += instruction
+    if position % 100 == 0:
+        total += 1
+
+print(total)
