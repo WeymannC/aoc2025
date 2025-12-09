fac162651bcf349069a2da3db3362e346f4e47ef
Christian Weymann <christian.weymann@tngtech.com>
2025-12-09 18:08:35 +0100

    Day 9 WIP

---
+++ 9.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+from functools import lru_cache
+from itertools import combinations, pairwise
+from math import floor, log10, ceil, prod
+
+from aocd import get_data
+
+data = get_data(day=9, year=2025)
+example = r"""7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3"""
+
+
+def parse_input(raw_input: str):
+    points = []
+    for line in raw_input.split("\n"):
+        x,y = line.split(",")
+        points.append((int(x), int(y)))
+    return points
+
+def rectangle_area(p, q):
+    x,y = p
+    r,s = q
+    return (abs(x-r)+1)*(abs(y-s)+1)
+
+def build_areas(points):
+    return [(rectangle_area(p,q), p, q) for p, q in combinations(points, 2)]
+
+def part1(used_input: str):
+    points = parse_input(used_input)
+    areas = build_areas(points)
+    return max(area for area, _, _ in areas)
+
+
+def part2(used_input: str):
+    points = parse_input(used_input)
+    areas = build_areas(points)
+    valid_areas = []
+    for area, p, q in areas:
+        x, y = p
+        r, s = q
+        if any((min(x,r) < u < max(x,r)) and (min(y,s) < v < max(y,s)) for u, v in points):
+            continue
+        valid_areas.append((area, p, q))
+    return max(area for area, _, _ in valid_areas)
+
+print(part1(data))
+
+print(part2(example))
