201b36c61bb25836d4f0328dce41e76372941cb6
Christian Weymann <christian.weymann@tngtech.com>
2025-12-12 16:40:55 +0100

    Day 10

--- 10.py
+++ 10.py
@@ -7,6 +7,8 @@ from math import floor, log10, ceil, prod
 from platform import machine
 
 from aocd import get_data
+from pulp import LpVariable, LpProblem, LpMinimize, lpSum, value
+from sympy import Matrix
 
 data = get_data(day=10, year=2025)
 example = r"""[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
@@ -38,34 +40,20 @@ def part1(used_input: str):
     machines = parse_input(used_input)
     return sum(least_presses_lights(tuple(False for _ in lights), lights, buttons) for lights, buttons, _ in machines)
 
-def least_presses_joltages(initial_state, target, buttons) -> int:
-    buttons_for_action = {i: [button for button in buttons if i in button] for i in range(len(initial_state))}
-    def next_action(state):
-        joltages_to_move = [i for i, (s_j, t_j) in enumerate(zip(state, target)) if s_j - t_j != 0]
-        next_action = min(joltages_to_move, key=lambda j: len(buttons_for_action[j]))
-        if len(actions:=buttons_for_action[next_action]) == 1:
-            return [(actions[0], target[next_action] - state[next_action])]
-        return [(action, 1) for action in actions]
-
-    queue = deque()
-    min_counter = math.inf
-    queue.extend((initial_state, action, 0) for action in next_action(initial_state))
-    while queue:
-        s, (a, n), counter = queue.pop()
-        if counter + n >= min_counter:
-            continue
-        new_state = tuple( joltage+n if i in a else joltage for i, joltage in enumerate(s))
-        if new_state == target:
-            min_counter = counter+n
-            continue
-        if any(new_state_j > target_j for new_state_j, target_j in zip(new_state, target)):
-            continue
-        queue.extend((new_state, action, counter + n) for action in next_action(new_state))
-    return min_counter
-
 def part2(used_input: str):
     machines = parse_input(used_input)
-    return sum(least_presses_joltages(tuple(0 for _ in joltages), joltages, buttons) for _, buttons, joltages in machines)
+    total = 0
+    for _, buttons, joltages in machines:
+        variables = [LpVariable(str(button), 0, cat="Integer") for button in buttons]
+        problem = LpProblem("problem", LpMinimize)
+        problem += lpSum(variables)
+        for i, joltage in enumerate(joltages):
+            problem += lpSum(variable for variable, button in zip(variables, buttons) if i in button) == joltage
+        problem.solve()
+        total += sum(value(variable) for variable in variables)
+
+
+    return total
 
 # print(part1(data))
 
